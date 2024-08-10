e4fe7ebd29c47151a84e7c2d4ddb0a97fc5edb94
Xu Yiming <xuyiming.open@outlook.com>
2024-08-10 15:24:11 +0800

    build: Use callback config

--- rspack.config.js
+++ rspack.config.js
@@ -3,7 +3,7 @@ import { rspack } from "@rspack/core";
 
 const targets = ["chrome >= 87", "edge >= 88", "firefox >= 78", "safari >= 14"];
 
-export default defineConfig({
+const config = defineConfig({
 	entry: {
 		main: "./src/main.js"
 	},
@@ -41,3 +41,19 @@ export default defineConfig({
 		css: true
 	}
 });
+
+/**
+ * @param {Env | undefined} env
+ * @param {Record<string, any> & { env: Env }} argv
+ * @returns {import("@rspack/core").RspackOptions}
+ */
+export default (env, argv) => {
+	return config;
+}
+
+/**
+ * @typedef Env
+ * @property {boolean} [RSPACK_BUNDLE]
+ * @property {boolean} [RSPACK_BUILD]
+ * @property {boolean} [RSPACK_WATCH]
+ */
